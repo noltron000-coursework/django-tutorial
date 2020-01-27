ad1c5d4d3a91a4d96c6798d67594ea2c860a3029
Nolan Kovacik <noltron000@gmail.com>
2020-01-27 15:56:26 -0800

    [add] model + 1st data item

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+	def __repr__(self):
+		return self.question_text
+
+	# create fields
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+	def __repr__(self):
+		return self.choice_text
+
+	# create fields
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
