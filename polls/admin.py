db3fb13f65ec31f48c0a73227de76ce159df60cc
Nolan Kovacik <noltron000@gmail.com>
2020-02-26 18:04:06 -0800

    [add] admin panel UI

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
 	model = Choice
 	extra = 3
 
@@ -13,7 +13,8 @@ class QuestionAdmin(admin.ModelAdmin):
 		(None,               {'fields': ['question_text']}),
 		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
-
 	inlines = [ChoiceInline]
+	list_filter = ['pub_date']
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -15,9 +15,15 @@ class Question(models.Model):
 		now = timezone.now()
 		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-	# create fields
+
+	# create fields.
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+
+	# add some admin properties.
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
 	def __repr__(self):
